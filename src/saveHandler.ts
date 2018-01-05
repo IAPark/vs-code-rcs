9ee4e3413afd41781c3a44e9df9388e3759595ae
Isaac Park <isaac.a.park@gmail.com>
2018-01-04 22:56:27 -0500

    Prevent edits to non unlocked files

--- src/saveHandler.ts
+++ src/saveHandler.ts
@@ -1,4 +1,4 @@
-import { Disposable, workspace, TextDocumentChangeEvent, window } from 'vscode';
+import { Disposable, workspace, TextDocumentChangeEvent, window, commands } from 'vscode';
 import { getInfo, lock, getHead } from './rcs';
 const username: () => Promise<string> = require('username')
 
@@ -14,11 +14,13 @@ export class SaveHandler implements Disposable {
         if (!e.document.isDirty) return; // if the document isn't dirty the change was made in the file system
         let info = await getInfo(e.document.uri.fsPath);
 
-            
         if (!info.state.locked && info.state.locker != await username()) {
+            commands.executeCommand<void>('undo');
+            
             window.showWarningMessage('Document not locked by you',  'Lock')
                 .then(option => {
                     if (option == 'Lock') {
+
                         lock(e.document.uri.fsPath);
                     }
                 })
